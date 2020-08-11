d45ea28549ed48b03c3c161fc5e3bcc44a4ef0c7
Shay Snyder <shaymeister@hotmail.com>
2020-08-10 21:42:21 -0400

    updated .gitignore, Vision/ README.md, train.py

---
+++ source/vision/train/train.py
@@ -0,0 +1,14 @@
+#pylint: disable=wrong-import-position
+
+from pycocotools.coco import COCO
+
+#pylint: enable=wrong-import-position
+
+# TODO: Finish Implementation
+def main():
+    print("Hello World")
+
+# if manage.py is being executed directly from
+# the CMD-Line, call the main function
+if __name__ == "__main__":
+    main()
