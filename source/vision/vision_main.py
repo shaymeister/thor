bd7edcc4ff16732a6b8395bbf80623ee360e0b5c
Shay Snyder <shaymeister@hotmail.com>
2020-08-17 17:01:59 -0400

    added framework for argparse and regex control

--- source/vision/vision_main.py
+++ source/vision/vision_main.py
@@ -1,6 +1,6 @@
 import argparse
 
-def create_parser():
+def create_argparser():
     """ Create the main parser for the Vision Pkg
 
     Returns
@@ -14,4 +14,23 @@ def create_parser():
     parser.add_argument('--train',
                         dest = 'train',
                         action = 'store_true',
-                        help = "Start S"
+                        help = "Start S")
+
+def create_regex_parser():
+    # TODO Finish Implementation
+    print("WARNING: this feature is not currently enabled. To use Vision, "
+        + "execute vision_main.py directly from cmd line")
+
+
+def main(flag):
+    if flag:
+        create_argparser
+    else:
+        create_regex_parser
+
+# Determine if vision_main.py is being executed
+# directly or from another script
+if __name__ == "__main__":
+    main(True) # executed directly
+else:
+    main(False) # executed indirectly
